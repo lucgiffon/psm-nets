1534c07db2cb7196e74926a366462928b31659a0
Luc Giffon <luc.giffon@lis-lab.fr>
2019-09-18 10:37:50 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Applying PALM to neural network to express layers as fast-transform',
+    author='Luc Giffon',
+    license='MIT',
+)
