1f49faff3bc1c1a0aa2853a80e4842b0032ee86a
lucgiffon <luc.giffon@gmail.com>
2021-05-28 18:30:25 +0200

    Update setup.py
    remove scikit-luc dep

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ AUTHOR = 'Luc Giffon'
 AUTHOR_EMAIL = ('luc.giffon@lis-lab.fr')
 INSTALL_REQUIRES = ['numpy', 'daiquiri', 'matplotlib', 'pandas',
                     'docopt', 'pillow', 'scikit-learn', 'psutil', 'yafe', "click", "python-dotenv",
-                    'xarray', 'keras', 'scipy==1.2.1', 'scikit-luc==2', 'tensorflow-model-optimization', 'scikit-image',
+                    'xarray', 'keras', 'scipy==1.2.1', 'tensorflow-model-optimization', 'scikit-image',
                     'pyfaust']
 EXTRAS_REQUIRE = {
     'dev': ['coverage', 'pytest', 'pytest-cov', 'pytest-randomly'],
